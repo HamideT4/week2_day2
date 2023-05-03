2c9a0850294c06fd66a3ff91890e10f9a6b2ca15
HamideT4 <hamidemahamat20@gmail.com>
2023-05-03 11:55:18 +0100

    programme renvoyant une erreur qu'un string non fini a rencontré une fin de ligne

---
+++ exo_04.rb
@@ -0,0 +1,6 @@
+#Le code renvoie une erreur qu'un string non fini a rencontré une fin de ligne 
+#exo_04.rb:1: unterminated string meets end of file
+#puts "Salut, ça farte ?
+#Pour que le programme marche, on doit fermer les quotes à la fin de la ligne
+
+puts "Salut, ça farte ?"
