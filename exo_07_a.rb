dce33d08a356effb1df57dbc0195e76ec7962ead
HamideT4 <hamidemahamat20@gmail.com>
2023-05-03 12:03:16 +0100

    programme introduisant la notion de recuperation de données saisies par l'utilisateur en ruby

---
+++ exo_07_a.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, c'est quoi ton blase ?"
+
+user_name = gets.chomp
+
+puts user_name
+
+#gets.chomp permet de recuperer la valeur d'une variable entrée au clavier
---
+++ exo_07_c.rb
@@ -0,0 +1,8 @@
+user_name = gets.chomp
+
+puts user_name
+
+#La difference entre ces trois codes est que le premier contient un message
+# qui informe l'utilisateur de ce qu'il doit saisir
+#Le second pareil mais ajoute une ligne où l'utilisateur doit saisir 
+#Le dernier enfin ne contient aucun message indiquant ce qu'on doit saisir
