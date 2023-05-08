f928e2d284037171f2095711c11a5ad53a840e07
HamideT4 <hamidemahamat20@gmail.com>
2023-05-08 10:48:57 +0100

    programme qui demande à l'utilisateur un nombre entre 1 et 25 et qui va sortir une pyramide qui montera au lieu de descendre

---
+++ pyramide.rb
@@ -0,0 +1,12 @@
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+print "> "
+number = gets.chomp.to_i
+while number < 1 or number > 25
+    puts "Le nombre d'étages doit être compris entre 1 et 25"
+    print "> "
+    number = gets.chomp.to_i
+end
+
+for n in 0...number
+    puts "#{' '*(number-n) + '#'*(n+1)}"
+end
